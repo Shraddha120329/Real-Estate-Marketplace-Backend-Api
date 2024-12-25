4e781864697bba2f06f27ad5fe175d3e79bc375f
Shraddha120329 <shraddhayadav1203@gmail.com>
2024-12-25 16:58:21 +0530

    commit

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -8,9 +8,13 @@ const userSchema = new mongoose.Schema(
       unique: true,
     },
     email: {
-      type: String,
-      required: true,
-      unique: true,
+    type: String,
+    required: [true, "Please provide e-mail"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide valid email",
+    ],
+    unique: true,
     },
     password: {
       type: String,
